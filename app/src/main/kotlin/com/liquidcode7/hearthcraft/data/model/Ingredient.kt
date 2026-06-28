ebab0f77f44fb8f29ee65f5574479a85e577c996
liquidcode7 <codinggithub@proton.me>
2026-06-28 14:13:35 +0100

    [hc] Data model: add gatherType, processType, processInputs fields to Ingredient

--- app/src/main/kotlin/com/liquidcode7/hearthcraft/data/model/Ingredient.kt
+++ app/src/main/kotlin/com/liquidcode7/hearthcraft/data/model/Ingredient.kt
@@ -3,6 +3,9 @@ package com.liquidcode7.hearthcraft.data.model
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class ProcessInput(val id: String, val qty: Int)
+
+@Serializable
 data class Ingredient(
     val id: String,
     val name: String,
@@ -10,6 +13,9 @@ data class Ingredient(
     val rarity: String = "c",
     val source: String = "forage",
     val gatheringMode: String = "forage",
+    val gatherType: String? = null,
+    val processType: String? = null,
+    val processInputs: List<ProcessInput>? = null,
     val primaryStat: String? = null,
     val secondaryStat: String? = null,
     val hazardTendency: String? = null,
